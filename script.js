3c65e073c3a779b603560e481c3e9450e158fefc
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2021-08-20 14:25:59 +0530

    done layout

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-document.querySelector("#test").innerText = "test successfulull"
+let initialPrice = document.querySelector("#buy-price")
+let stocksQuantity = document.querySelector("#stocks-qty")
+let currentPrice = document.querySelector("#current-price")
