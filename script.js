249366356c07586fbcbf479609013c777e76f176
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2021-08-21 11:35:05 +0530

    complete project

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
 let initialPrice = document.querySelector("#buy-price")
 let stocksQuantity = document.querySelector("#stocks-qty")
 let currentPrice = document.querySelector("#current-price")
+let submitBtn = document.querySelector("#submit-btn")
+let outputDiv = document.querySelector("#output")
+
+
+submitBtn.addEventListener('click', buttonHandler);
+
+function buttonHandler(e){
+    e.preventDefault();
+    let ip = Number(initialPrice.value)
+    let qty = Number(stocksQuantity.value)
+    let cp = Number(currentPrice.value)
+
+    if(ip === 0 || qty === 0 || cp === 0){
+        outputDiv.innerHTML = "<p>Please fill all the fields.</p>"
+    }
+    else{
+        calculateProfitAndLoss(ip, qty, cp)
+    }
+}
+
+function calculateProfitAndLoss(initial, quantity, current){
+    if(current > initial){
+        let profit = (current - initial) * quantity
+        let profitPercentage = (profit / initial) *100
+        outputDiv.style.color = "green"
+        outputDiv.innerHTML = "Your profit is "+ profit.toFixed(2) +" and profit percentage is "+ profitPercentage.toFixed(2) + "%"
+    }else if(current < initial){
+        let loss = (initial - current) * quantity
+        let lossPrecentage = (loss / initial) *100
+        outputDiv.style.color = "red"
+        outputDiv.innerHTML = "Your loss is "+ loss.toFixed(2) +" and loss percentage is "+ lossPrecentage.toFixed(2) + "%"
+    }else{
+        outputDiv.innerHTML = "<p>The world is round boss. Neither profit, nor loss.</p>"
+    }
+}
+
